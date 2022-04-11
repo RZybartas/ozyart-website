c41e83bddbda91f1128a658d10f17fb79bbd0981
Ramunas Zybartas <ramunzzz007@yahoo.es>
2022-04-11 14:02:24 +0300

    feat/created not found page

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 import { Dashboard } from './pages/Dashboard';
 import { Login } from './pages/Login';
+import { NotFound } from './pages/NotFound';
 import { Products } from './pages/Products';
 import { Register } from './pages/Register';
 import { Users } from './pages/Users';
@@ -17,6 +18,7 @@ const App = () => {
           <Route path='/login' element={<Login />} />
           <Route path='/users' element={<Users />} />
           <Route path='/products' element={<Products />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </Router>
     </>
