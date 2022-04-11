3da4b3b5eff184bee2528269483370be3514b657
Ramunas Zybartas <ramunzzz007@yahoo.es>
2022-04-11 13:26:03 +0300

    feat/created users page

--- admin/src/App.js
+++ admin/src/App.js
@@ -3,6 +3,7 @@ import { Navbar } from './components/Navbar';
 import { Dashboard } from './pages/Dashboard';
 import { Login } from './pages/Login';
 import { Register } from './pages/Register';
+import { Users } from './pages/Users';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
           <Route path='/' element={<Dashboard />} />
           <Route path='/register' element={<Register />} />
           <Route path='/login' element={<Login />} />
+          <Route path='/users' element={<Users />} />
         </Routes>
       </Router>
     </>
---
+++ admin/src/pages/Users.js
@@ -0,0 +1,54 @@
+import { DataGrid } from '@mui/x-data-grid';
+import { useSelector } from 'react-redux';
+import { Sidebar } from '../components/Sidebar';
+
+const columns = [
+  { field: '_id', headerName: 'ID', width: 210 },
+  { field: 'username', headerName: 'Username', width: 150 },
+  { field: 'email', headerName: 'Email', width: 250 },
+  {
+    field: 'createdAt',
+    headerName: 'Created',
+    type: 'date',
+    width: 210,
+  },
+  {
+    field: 'isAdmin',
+    headerName: 'Role',
+    width: 100,
+    editable: true,
+  },
+  {
+    field: 'updatedAt',
+    headerName: 'Updated',
+    width: 120,
+  },
+];
+
+export const Users = () => {
+  const { users } = useSelector((state) => state.users);
+
+  return (
+    <div className='users'>
+      <div className='users__sidebar'>
+        <Sidebar />
+      </div>
+      <section className='users__wrapper'>
+        <h1 className='users__title'>Users</h1>
+        <div className='users__table'>
+          {users ? (
+            <DataGrid
+              getRowId={(row) => row._id}
+              rows={users}
+              columns={columns}
+              pageSize={5}
+              rowsPerPageOptions={[5]}
+            />
+          ) : (
+            <h2 className='users__message'>Sorry, you don't have permision</h2>
+          )}
+        </div>
+      </section>
+    </div>
+  );
+};
