80289f63935f864b200c22d5439f08d1e4c93646
Ramunas Zybartas <ramunzzz007@yahoo.es>
2022-04-14 16:32:27 +0300

    feat/created notfound page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Sidebar } from './components/Sidebar';
 import { Home } from './pages/Home';
+import { NotFound } from './pages/NotFound';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Sidebar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </Router>
   );
---
+++ client/src/pages/NotFound.js
@@ -0,0 +1,14 @@
+import { useNavigate } from 'react-router-dom';
+
+export const NotFound = () => {
+  const navigate = useNavigate();
+  return (
+    <div className='not-found-container'>
+      <div className='not-found-btn'>
+        <button onClick={() => navigate(-1)}>back</button>
+      </div>
+      <h1 className='not-found-title'>Page not found</h1>
+      <p className='not-found-paragraph'>404</p>
+    </div>
+  );
+};
