107c059bd3d79dc3f0514b52141257c7a4cac396
Ramunas Zybartas <ramunzzz007@yahoo.es>
2022-05-05 12:33:30 +0300

    fix/changed logo in client

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@ export const Header = () => {
     <header className='header'>
       <div className='header__container'>
         <img
-          src='https://res.cloudinary.com/dl8kybvwx/image/upload/v1648558891/mtrlkjauu2sd30kocqwo.svg'
+          src='https://res.cloudinary.com/dl8kybvwx/image/upload/v1651598058/kzxevp8wfnxcjfie4vs5.svg'
           alt='ozyart-logo'
           className='header__logo'
         />
